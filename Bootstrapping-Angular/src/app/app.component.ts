949ee4e4860ca69d61ade78bc879fca25defe623
F3us2022 <arsenal0288@gmail.com>
2022-05-04 07:49:06 -0400

    v1.1 commit

--- Bootstrapping-Angular/src/app/app.component.ts
+++ Bootstrapping-Angular/src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  //template: `<p>Hello App</p>`,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Bootstrapping-Angular';
-}
+export class AppComponent {}
