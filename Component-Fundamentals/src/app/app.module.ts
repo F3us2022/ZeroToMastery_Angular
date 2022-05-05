7cd020e1037dbfe500ab41b5b61e4200fa52aed6
F3us2022 <arsenal0288@gmail.com>
2022-05-05 18:52:13 -0400

    v1.1 commit

--- Component-Fundamentals/src/app/app.module.ts
+++ Component-Fundamentals/src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, PostComponent],
+  imports: [BrowserModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
