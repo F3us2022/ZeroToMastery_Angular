fb97ceb948bd23508a70f0aeb018e3332001ca1a
F3us2022 <arsenal0288@gmail.com>
2022-05-11 23:48:59 -0400

    v1.1 commit

--- Featured-Module/src/app/product/product/product.component.ts
+++ Featured-Module/src/app/product/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { filter, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product',
@@ -6,7 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
+  private destroyObserv!: Subscription;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    //PROMISE
+    const pr = new Promise((res) => {
+      setTimeout(() => {
+        res('Promise Resolved-1');
+        res('Promise Resolved-2');
+      }, 1000);
+    });
+
+    pr.then((data) => console.log(data));
+
+    //OBSERVABLE
+    const ob = new Observable((sub) => {
+      // setTimeout(() => {
+      //   sub.next('Observable Subscribed-1');
+      //   sub.next('Observable Subscribed-2');
+      // }, 1000);
+
+      setInterval(() => {
+        sub.next('Observable Subscribed-1');
+      }, 1000);
+    });
+
+    // ob.pipe(filter((d) => d === 'Observable Subscribed-2')).subscribe((data) =>
+    //   console.log(data)
+    // );
+    //ob.subscribe((data) => console.log(data));
+
+    this.destroyObserv = ob.subscribe((data) => console.log(data));
+  }
+
+  ngOnDestroy() {
+    this.destroyObserv.unsubscribe();
+  }
 }
