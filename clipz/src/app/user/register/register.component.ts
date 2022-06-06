ed6f222bb4008f876b25f712aaddd90052a3e378
F3us2022 <arsenal0288@gmail.com>
2022-06-06 06:51:56 -0400

    v1.1 commit

--- clipz/src/app/user/register/register.component.ts
+++ clipz/src/app/user/register/register.component.ts
@@ -7,11 +7,22 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  email = new FormControl('');
-  age = new FormControl('');
-  password = new FormControl('');
-  confirm_password = new FormControl('');
-  phoneNumber = new FormControl('');
+  email = new FormControl('', [Validators.required, Validators.email]);
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(100),
+  ]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
+  ]);
+  confirm_password = new FormControl('', [Validators.required]);
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.min(13),
+    Validators.max(13),
+  ]);
 
   registerForm = new FormGroup({
     name: this.name,
