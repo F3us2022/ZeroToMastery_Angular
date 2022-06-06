70779e6cddacd2b61509f6706cd67477e04dbe67
F3us2022 <arsenal0288@gmail.com>
2022-06-06 06:56:00 -0400

    v1.2 commit

--- clipz/src/app/user/register/register.component.ts
+++ clipz/src/app/user/register/register.component.ts
@@ -1,30 +1,37 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  UntypedFormGroup,
+  UntypedFormControl,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  email = new FormControl('', [Validators.required, Validators.email]);
-  age = new FormControl('', [
+  name = new UntypedFormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+  email = new UntypedFormControl('', [Validators.required, Validators.email]);
+  age = new UntypedFormControl('', [
     Validators.required,
     Validators.min(18),
     Validators.max(100),
   ]);
-  password = new FormControl('', [
+  password = new UntypedFormControl('', [
     Validators.required,
     Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
   ]);
-  confirm_password = new FormControl('', [Validators.required]);
-  phoneNumber = new FormControl('', [
+  confirm_password = new UntypedFormControl('', [Validators.required]);
+  phoneNumber = new UntypedFormControl('', [
     Validators.required,
     Validators.min(13),
     Validators.max(13),
   ]);
 
-  registerForm = new FormGroup({
+  registerForm = new UntypedFormGroup({
     name: this.name,
     email: this.email,
     age: this.age,
