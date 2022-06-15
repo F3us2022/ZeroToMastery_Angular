720083ecbd989ab45cc7e28ad86118a769e49d97
F3us2022 <arsenal0288@gmail.com>
2022-06-15 04:09:31 -0400

    v1.1 commit

--- Intro-to-RxJS/dist/src.e31bb0bc.js
+++ Intro-to-RxJS/dist/src.e31bb0bc.js
@@ -11176,13 +11176,52 @@ var _zipWith = require("./internal/operators/zipWith");
 var _rxjs = require("rxjs");
 
 //Observable Example
-var obs = new _rxjs.Observable(function (subscriber) {
-  subscriber.next('Data Stream One');
-  subscriber.next('Data Stream two');
-  subscriber.complete();
-  subscriber.next('Data Stream Three');
-  subscriber.error('test error');
-});
+// const obs = new Observable((subscriber) => {
+//     subscriber.next('Data Stream One');
+//     subscriber.next('Data Stream two');
+//     subscriber.complete();
+//     subscriber.next('Data Stream Three');
+//     subscriber.error('test error');
+//  });
+// obs.subscribe({
+//     next: (value) => {
+//         console.log(value)
+//     },
+//     complete: () => { console.log('complete called') },
+//     error: (err)=>{console.log(err)}
+// })
+//----------------------------------------- Synchronous Observable ----------------------------------------------------
+
+/*
+
+const obs = new Observable((subscriber) => {
+    subscriber.next('Data Stream One');
+    subscriber.next('Data Stream two');
+    subscriber.complete();
+    subscriber.next('Data Stream Three');
+    subscriber.error('test error');
+ });
+
+console.log('Before');
+
+obs.subscribe({
+    next: (value) => {
+        console.log(value)
+    },
+    complete: () => { console.log('complete called') },
+    error: (err)=>{console.log(err)}
+})
+
+
+console.log('After');
+
+*/
+//
+//
+//----------------------------------------- ASynchronous Observable ----------------------------------------------------
+//
+var obs = new _rxjs.Observable(function (subscriber) {});
+console.log('Before');
 obs.subscribe({
   next: function next(value) {
     console.log(value);
--- Intro-to-RxJS/src/index.js
+++ Intro-to-RxJS/src/index.js
@@ -2,6 +2,27 @@
 
 import { Observable } from 'rxjs';
 
+// const obs = new Observable((subscriber) => {
+//     subscriber.next('Data Stream One');
+//     subscriber.next('Data Stream two');
+//     subscriber.complete();
+//     subscriber.next('Data Stream Three');
+//     subscriber.error('test error');
+//  });
+
+// obs.subscribe({
+//     next: (value) => {
+//         console.log(value)
+//     },
+//     complete: () => { console.log('complete called') },
+//     error: (err)=>{console.log(err)}
+// })
+
+
+//----------------------------------------- Synchronous Observable ----------------------------------------------------
+
+/*
+
 const obs = new Observable((subscriber) => {
     subscriber.next('Data Stream One');
     subscriber.next('Data Stream two');
@@ -10,6 +31,8 @@ const obs = new Observable((subscriber) => {
     subscriber.error('test error');
  });
 
+console.log('Before');
+
 obs.subscribe({
     next: (value) => {
         console.log(value)
@@ -17,3 +40,33 @@ obs.subscribe({
     complete: () => { console.log('complete called') },
     error: (err)=>{console.log(err)}
 })
+
+
+console.log('After');
+
+*/
+
+//
+
+//
+
+
+//----------------------------------------- ASynchronous Observable ----------------------------------------------------
+
+//
+
+
+const obs = new Observable((subscriber) => {
+    
+ });
+
+console.log('Before');
+
+obs.subscribe({
+    next: (value) => {
+        console.log(value)
+    },
+    complete: () => { console.log('complete called') },
+    error: (err)=>{console.log(err)}
+})
+
