497d55183a144e70fa69aa0c8a310709645965ed
F3us2022 <arsenal0288@gmail.com>
2022-06-15 06:45:29 -0400

    v1.1 commit

--- Intro-to-RxJS/src/index.js
+++ Intro-to-RxJS/src/index.js
@@ -20,7 +20,6 @@
 
 //Observable Example
 
-import { Observable } from 'rxjs';
 
 // const obs = new Observable((subscriber) => {
 //     subscriber.next('Data Stream One');
@@ -127,23 +126,66 @@ console.log('After');
 
 //------------------------------------- ASynchronous Observable (UNSUBSCRIBE)-------------------------------------------
 
-const obs = new Observable((subscriber) => {
-    const id = setInterval(() => {
-        subscriber.next('text')
-    console.log(`memory leak even after 'subscriber.complete' is called`)}, 1000) //memory leak
+// const obs = new Observable((subscriber) => {
+//     const id = setInterval(() => {
+//         subscriber.next('text')
+//     console.log(`memory leak even after 'subscriber.complete' is called`)}, 1000) //memory leak
 
-    return ()=>{ 
-        clearInterval(id);
-    }
- });
+//     return ()=>{ 
+//         clearInterval(id);
+//     }
+//  });
 
 
-const subscriber = obs.subscribe({
-    next: (value) => {
-        console.log(value)
-    },
-    complete: () => { console.log('complete called') },
-    error: (err)=>{console.log(err)}
-})
+// const subscriber = obs.subscribe({
+//     next: (value) => {
+//         console.log(value)
+//     },
+//     complete: () => { console.log('complete called') },
+//     error: (err)=>{console.log(err)}
+// })
 
-setTimeout(() => {subscriber.unsubscribe()},4000)
+// setTimeout(() => {subscriber.unsubscribe()},4000)
+
+//
+
+//
+
+
+//------------------------------------- Declarative Programming with OPERATORS-------------------------------------------
+
+
+//
+
+//------------------- Use of OPERATOR INTERVAL -----------
+
+// import { interval } from 'rxjs';
+
+// const obs = new interval(1000);
+
+
+// const subscriber = obs.subscribe(
+//     console.log
+// )
+
+//------------------- Use of OPERATOR TIMER --------------
+
+// import { timer } from 'rxjs'
+
+// const obs = new timer(0, 1000);
+
+// const subscriber = obs.subscribe(console.log);
+
+//
+
+//--------------------------------------------------- DOM Event OPERATORS-------------------------------------------------
+
+//
+
+// import { from, fromEvent } from 'rxjs'
+
+// const obs = new fromEvent(
+//     document, 'click'
+// );
+
+// const subscriber = obs.subscribe(console.log);
