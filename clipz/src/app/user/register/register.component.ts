f63f66aa40623e4404ac4b977911f13e5793f8d7
F3us2022 <arsenal0288@gmail.com>
2022-06-18 01:41:46 -0400

    v1.2 commit

--- clipz/src/app/user/register/register.component.ts
+++ clipz/src/app/user/register/register.component.ts
@@ -6,19 +6,25 @@ import {
 } from '@angular/forms';
 
 import { AuthService } from 'src/app/services/auth.service';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private emailTaken: EmailTaken) {}
   inSubmission = false;
   name = new UntypedFormControl('', [
     Validators.required,
     Validators.minLength(3),
   ]);
-  email = new UntypedFormControl('', [Validators.required, Validators.email]);
+  email = new UntypedFormControl(
+    '',
+    [Validators.required, Validators.email],
+    [this.emailTaken.validate]
+  );
   age = new UntypedFormControl('', [
     Validators.required,
     Validators.min(18),
@@ -35,14 +41,17 @@ export class RegisterComponent {
     Validators.maxLength(10),
   ]);
 
-  registerForm = new UntypedFormGroup({
-    name: this.name,
-    email: this.email,
-    age: this.age,
-    password: this.password,
-    confirm_password: this.confirm_password,
-    phoneNumber: this.phoneNumber,
-  });
+  registerForm = new UntypedFormGroup(
+    {
+      name: this.name,
+      email: this.email,
+      age: this.age,
+      password: this.password,
+      confirm_password: this.confirm_password,
+      phoneNumber: this.phoneNumber,
+    },
+    [RegisterValidators.match('password', 'confirm_password')]
+  );
 
   showAlert = false;
   alertMsg = 'Please wait! Your account is getting created.';
