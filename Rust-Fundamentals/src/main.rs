d65c7ba4a57b937c0c30e973b0526f7f82658881
F3us2022 <arsenal0288@gmail.com>
2022-06-20 06:21:25 -0400

    v1.1 commit

--- Rust-Fundamentals/src/main.rs
+++ Rust-Fundamentals/src/main.rs
@@ -73,37 +73,40 @@
 
 //------------------------------------------------------------------ ARRAYS ----------------------------------------------------------------------
 
-fn add(num_one:i32 , num_two:i32) -> i32 {
-    num_one+num_two
+    // fn main(){
+    //     let arr:[i32;5] = [1,2,3,4,5];
+    //     println!("{:?}",arr);
+    // }
+
+//------------------------------------------------------------------ VECTOR ----------------------------------------------------------------------
+
+    
+    // fn main(){
+    //     let vector_items = vec![1,2,3,4,5];
+    //     let mut vector_items_2 = Vec::new();
+    //     vector_items_2.push(1);
+    //     vector_items_2.push(2);
+    //     vector_items_2.push(3);
+    //     vector_items_2.push(4);
+    //     vector_items_2.push(5);
+     
+    //     println!("{:?}",vector_items);
+    //     println!("{:?}",vector_items_2);
+    // }
+
+//------------------------------------------------------------------ STRUCTURE ----------------------------------------------------------------------
+
+struct BankAccount {
+    balance:i32,
+    verified:bool
 }
-    fn main(){
-        let mut total:i32;
-        let  free_shipping=false;
-        total = add(10,3);
-        if total > 50{
-            println!("You qualify for free shipping.");
-        }
-        else if total > 25
-        {
-            println!("If you add more items, You will qualify for free shipping.");
-        }
-        else {
-            println!("No free Shipping.");
-        }
+fn main(){
 
-        // match free_shipping{
-        //     true=> total = total + 0,
-        //     false=> total = total + 5
-        // }
+    let my_account = BankAccount{
+        balance:2000,
+        verified:true
+    };
 
-        total =  match free_shipping{
-            true=> total + 0,
-            false=> total + 5
-        };
-        match total{
-            1 => println!("1"),
-            2 => println!("2"),
-            _ => println!("No match foud!")
-        }
-        println!("{0}",total);
-    }
+    println!("{:?}",my_account.balance);
+    println!("{:?}",my_account.verified)
+}
