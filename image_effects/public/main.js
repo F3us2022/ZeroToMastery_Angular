960016c3cabcae1c1d73ad502552d53a499a46e8
F3us2022 <arsenal0288@gmail.com>
2022-06-22 10:28:19 -0400

    v1.2 commit

--- image_effects/public/main.js
+++ image_effects/public/main.js
@@ -18,7 +18,8 @@ async function init() {
 
         let base64 = fileReader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/,'');
         console.log(input.files[0]);
-        rustApp.grayscale(base64);
+        let img_data_url = rustApp.grayscale(base64);
+        document.getElementById('new-img').setAttribute('src',img_data_url)
     }
     input.addEventListener('change', () => {
         fileReader.readAsDataURL(input.files[0]);
--- image_effects/src/lib.rs
+++ image_effects/src/lib.rs
@@ -1,11 +1,12 @@
 
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
-use base64::decode;
+use base64::{encode,decode};
 use image::load_from_memory;
+use image::ImageOutputFormat::Png;
 
 #[wasm_bindgen]
-pub fn grayscale(encoded_file:&str){
+pub fn grayscale(encoded_file:&str) -> String{
     log(&"Grayscale called".into());
 
     let base64_to_vector=decode(encoded_file).unwrap();
@@ -16,4 +17,13 @@ pub fn grayscale(encoded_file:&str){
 
     img = img.grayscale();
     log(&"Grayscale effect applied".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"New image writter".into());
+
+    let encoded_img=encode(&buffer);
+    let data_url = format!("data:image/png;base64,{}",encoded_img);
+
+    data_url
 } 
